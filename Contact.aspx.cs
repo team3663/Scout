b7832e147cdcaf2cb953d0486ec044ef15aab0d9
unknown <cpcssamuelwynsma@gmail.com>
2014-01-25 12:53:05 -0800

    The basic scouting code

---
+++ Contact.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Contact : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            System.IO.FileInfo MyFile = new System.IO.FileInfo(@"C:\Temp\Data.txt");
+            System.IO.StreamWriter MyStream = MyFile.CreateText();
+            String Banana = String.Format("Match Number: {0}", TextBox1.Text);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Team Number: {0}", TextBox2.Text);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("High Goal: {0}", CheckBoxList1.Items[0].Selected);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Low Goal: {0}", CheckBoxList1.Items[1].Selected);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Hot Goal: {0}", CheckBoxList1.Items[2].Selected);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Blocks: {0}", CheckBoxList1.Items[3].Selected);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Movement: {0}", CheckBoxList1.Items[4].Selected);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Goalie Box: {0}", PositionList.Items[0].Selected);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Defense: {0}", PositionList.Items[1].Selected);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Center: {0}", PositionList.Items[2].Selected);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Offense: {0}", PositionList.Items[3].Selected);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Out of play: {0}", PositionList.Items[4].Selected);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Speed: {0}", ServerControl13.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Manueverability: {0}", ServerControl12.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Driver Competence: {0}", ServerControl11.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Cycles: {0}", ServerControl14.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Truss Made: {0}", ServerControl15.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Truss Missed: {0}", ServerControl16.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Catch Made: {0}", ServerControl17.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Catch Missed: {0}", ServerControl18.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("High Goal Made: {0}", ServerControl19.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("High Goal Missed: {0}", ServerControl110.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Low Goal Made: {0}", ServerControl111.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Low Goal Missed: {0}", ServerControl112.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Assists Received: {0}", ServerControl113.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Assists Given: {0}", ServerControl114.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Passes Blocked: {0}", ServerControl115.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Shots Blocked: {0}", ServerControl116.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Fouls: {0}", ServerControl117.CurrentValue);
+            MyStream.WriteLine(Banana);
+            Banana = String.Format("Comments: {0}", CommentsBox.Text);
+            MyStream.WriteLine(Banana);
+
+            MyStream.Close();
+        }
+
+    }
+}
---
+++ ServerControl1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerControl1
+{
+    [DefaultProperty("Text")]
+    [ToolboxData("<{0}:ServerControl1 runat=server></{0}:ServerControl1>")]
+    public class ServerControl1 : CompositeControl
+    {
+        Label title;
+        Button MinusButton;
+        TextBox IntBox;
+        Button PlusButton;
+
+        [Bindable(true)]
+        [Category("Appearance")]
+        [DefaultValue("")]
+        [Localizable(true)]
+        public string Title
+        {
+            get
+            {
+                String s = (String)ViewState["Title"];
+                return ((s == null)? "Title" : s);
+            }
+ 
+            set
+            {
+                ViewState["Title"] = value;
+            }
+        }
+
+        [Bindable(true)]
+        [Category("Appearance")]
+        [DefaultValue("")]
+        [Localizable(true)]
+        public int MinimumValue
+        {
+            get
+            {
+                String s = (String)ViewState["MinimumValue"];
+                return ((s == null) ? 0 : int.Parse(s));
+            }
+
+            set
+            {
+                ViewState["MinimumValue"] = value.ToString();
+            }
+        }
+
+        [Bindable(true)]
+        [Category("Appearance")]
+        [DefaultValue("")]
+        [Localizable(true)]
+        public int MaximumValue
+        {
+            get
+            {
+                String s = (String)ViewState["MaximumValue"];
+                return ((s == null) ? 10 : int.Parse(s));
+            }
+
+            set
+            {
+                ViewState["MaximumValue"] = value.ToString();
+            }
+        }
+
+        [Bindable(true)]
+        [Category("Appearance")]
+        [DefaultValue("")]
+        [Localizable(true)]
+        public int CurrentValue
+        {
+            get
+            {
+                String s = (String)ViewState["CurrentValue"];
+                return ((s == null) ? 5 : int.Parse(s));
+            }
+
+            set
+            {
+                ViewState["CurrentValue"] = value.ToString();
+            }
+        }
+
+        protected override void CreateChildControls()
+        {
+            title = new Label();
+            title.Text = Title;
+            title.Width = 100;
+            Controls.Add(title);
+
+            Controls.Add(new LiteralControl("&nbsp; &nbsp;"));
+
+            MinusButton = new Button();
+            MinusButton.Text = "-";
+            MinusButton.BackColor = System.Drawing.Color.Red;
+            MinusButton.Click += MinusButton_Click;
+            MinusButton.Width = 30;
+            Controls.Add(MinusButton);
+
+            Controls.Add(new LiteralControl("&nbsp; &nbsp;"));
+
+            IntBox = new TextBox();
+            IntBox.Text = CurrentValue.ToString();
+            IntBox.Width = 50;
+            IntBox.ReadOnly = true;
+            Controls.Add(IntBox);
+
+            Controls.Add(new LiteralControl("&nbsp; &nbsp;"));
+
+            PlusButton = new Button();
+            PlusButton.Text = "+";
+            PlusButton.BackColor = System.Drawing.Color.Green;
+            PlusButton.Click += PlusButton_Click;
+            PlusButton.Width = 30;
+            Controls.Add(PlusButton);
+
+        }
+
+        protected void MinusButton_Click(object sender, EventArgs e)
+        {
+            int Temp = CurrentValue;
+            
+            if (Temp > MinimumValue)
+            {
+                CurrentValue = Temp - 1;
+            }
+            
+        }
+
+
+        protected void PlusButton_Click(object sender, EventArgs e)
+        {
+            int Temp = CurrentValue;
+            
+            if (Temp < MaximumValue)
+            {
+                CurrentValue = Temp + 1;
+            }
+        }
+ 
+    }
+}
